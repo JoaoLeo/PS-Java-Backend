9b4f8c90bc72f3860e2657d359b9313c4904a1ca
João Leonardo <joaoleonardo9921@gmail.com>
2023-07-13 09:55:39 -0300

    adicionando controller de conta

--- src/main/java/br/com/banco/controllers/ContaController.java
+++ src/main/java/br/com/banco/controllers/ContaController.java
@@ -1,2 +1,52 @@
-package br.com.banco.controllers;public class ContaController {
+package br.com.banco.controllers;
+
+import br.com.banco.models.Conta;
+import br.com.banco.services.ContaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/contas")
+public class ContaController {
+    @Autowired
+    private ContaService service;
+
+    @GetMapping
+    public ResponseEntity<List<Conta>> getAll() {
+        List<Conta> Contas = service.getAll();
+        return ResponseEntity.ok().body(Contas);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Conta> findById(@PathVariable Long id) {
+        Conta obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Conta> insert(@RequestBody Conta obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(obj.getIdConta())
+                .toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Conta> update(@PathVariable Long id,  @RequestBody Conta obj){
+        obj = service.update(id,obj);
+        return ResponseEntity.ok().body(obj);
+    }
 }
